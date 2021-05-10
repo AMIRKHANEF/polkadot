09b47a3888ed60ef2ebbb4c5ea51f87f7e05f925
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2021-05-10 15:57:16 +0430

    updated

--- dot/Dot.js
+++ dot/Dot.js
@@ -4,46 +4,52 @@ const { mnemonicGenerate } = require('@polkadot/util-crypto');
 const keyring = new Keyring({ type: 'sr25519'});
 var mongoose = require('mongoose');
 require('dotenv').config();
+const db = require('../data/database');
+const Wallet = require('../data/model/walletSchema');
 
 let wnd = 1000000000000;
 
-var WalletSchema = new mongoose.Schema({
-    coinSymbol: {
-        type: String,
-        trim: true
-    },
-    address: {
-        type: String,
-        trim: true
-    },
-    mnemonic: {
-        type: String,
-        trim: true
-    },
-    creationTime: Date
-});
-const Wallet = mongoose.model("senderInfo", WalletSchema);
+// var WalletSchema = new mongoose.Schema({
+//     coinSymbol: {
+//         type: String,
+//         trim: true
+//     },
+//     address: {
+//         type: String,
+//         trim: true
+//     },
+//     mnemonic: {
+//         type: String,
+//         trim: true
+//     },
+//     creationTime: Date
+// });
+// const Wallet = mongoose.model("senderInfo", WalletSchema);
 
-const url = process.env.DATABASE_URL;
-//connect to MongoDB
-mongoose.connect(url, {
-  //  useMongoClient: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+// const url = process.env.DATABASE_URL;
+// //connect to MongoDB
+// mongoose.connect(url, {
+//   //  useMongoClient: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
 
 
 module.exports = {
-    async createSenderAddr(_network){
+    async createAddr(_network, _save){
         try{
             const mnemonic = mnemonicGenerate();
-            const senderPair = keyring.addFromMnemonic(mnemonic);
-            console.log(` Sender address just created on ${_network}: ${senderPair.address}!`);
-            this.saveSenderInfo(mnemonic, senderPair.address);
+            const Pair = keyring.addFromMnemonic(mnemonic);
+            console.log(` Address just created on ${_network}: ${Pair.address}!`);
+            if (_save){
+            this.saveSenderInfo(mnemonic, Pair.address);
+            } else if (!_save){
+                return Pair.address;
+            }
         } catch (error){
-            console.log(` Error in "createSenderAddr" section!!!\n ${error}`);
+            console.log(` Error in "createAddr" section!!!\n ${error}`);
         }
     },
     saveSenderInfo(_mnemonic, _senderAddr){
@@ -68,44 +74,47 @@ module.exports = {
         const sender = keyring.addFromMnemonic(result[0].mnemonic);
         return sender;
     },
-    async createReceiverAddr(_network){
-        try{
-            const mnemonic = mnemonicGenerate();
-            const receiverPair = keyring.addFromMnemonic(mnemonic);
-            console.log(` Receiver address just created on ${_network}: ${receiverPair.address}!`);
-            return receiverPair.address;
-        } catch (error){
-            console.log(` Error in "createReceiverAddr" section!!!\n ${error}`);
-        }
-    },
     async signTx(_api, _sender, _receiver){
         try{
-            const tx = _api.tx.balances.transfer(_receiver, 10000000000);
+            const value = 10000000000;
+            const tx = _api.tx.balances.transfer(_receiver, value);
             console.log('signing transaction!');
-            const txHash = await tx.signAndSend(_sender, ({ status }) => {
-                console.log(` Current status is ${status.type}`);
-            if(status.type == 'Finalized'){
-                this.checkSenderInfo(_api, _sender.address);
-                this.checkReceiverInfo(_api, _receiver);
-            }});
+            const fee = await this.calculateFee(_api, tx, _sender, _receiver);
+            let balance = await this.getBalanceOf(_api, _sender.address);
+            if (balance >= (fee + (value / wnd))){
+                const txHash = await tx.signAndSend(_sender, ({ status }) => {
+                    console.log(` Current status is ${status.type}`);
+                    if(status.type == 'Finalized'){
+                        this.checkInfo(_api, _sender.address);
+                        this.checkInfo(_api, _receiver);
+                    }
+                });
+            } else {
+                console.log(' Not enough balance!!!');
+            }
         } catch (error){
             console.log(` Error while signing transaction!!!\n ${error}`);
         }
     },
-    async checkSenderInfo(_api, _senderAddr){
+    async checkInfo(_api, _Addr){
         try{
-            let { data: { free: previousFree }, nonce: previousNonce } = await _api.query.system.account(_senderAddr);
-            console.log(` ${_senderAddr} has a balance of ${previousFree / wnd} wnd, nonce ${previousNonce} !`);
+            let { data: { free: Free }, nonce: Nonce } = await _api.query.system.account(_Addr);
+            console.log(` ${_Addr} has a balance of ${Free / wnd} wnd, nonce ${Nonce} !`);
         } catch (error){
-            console.log(` Error in "checkSenderInfo" section!!\n ${error}`);
+            console.log(` Error in "checkInfo" section!!\n ${error}`);
         }
     },
-    async checkReceiverInfo(_api, _ReceiverAddr){
+    async calculateFee(_api, _tx, _sender, _receiver){
+        const fee = await _tx.paymentInfo(_sender);
+        console.log(` The transaction fee estimate is : ${fee.partialFee / wnd} wnd!`);
+        return (fee.partialFee) / wnd;
+    },
+    async getBalanceOf(_api, _addr){
         try{
-            let { data: { free: previousFree }, nonce: previousNonce } = await _api.query.system.account(_ReceiverAddr);
-            console.log(` ${_ReceiverAddr} has a balance of ${previousFree/ wnd} wnd, nonce ${previousNonce} !`);
-        } catch (error){
-            console.log(` Error in "checkReceiverInfo" section!!\n ${error}`);
+            let { data: { free: Free }} = await _api.query.system.account(_addr);
+            return Free / wnd;
+        } catch(err) {
+            console.log('error' + err);
         }
     }
 }
